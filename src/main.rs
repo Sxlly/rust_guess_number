46b171685a57e1bfd925d46dd214280bb383bc14
Sxlly <shaesullivan.buisness1@gmail.com>
2024-09-19 12:13:02 +0800

    pick a number

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,19 @@
 // COMP2007 - Curtin University
 // 19/09/2024
 
-
+use std::io::stdin;
 
 fn main() {
-    println!("Hello, world!");
+
+    outer_loop: loop {
+
+        let number_to_guess: u32 = 10;
+
+        println!("Please pick a number: ");
+
+        loop {
+            
+        }
+    }
+    
 }
